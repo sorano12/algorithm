8a147fdd84ec81f424260034a74e37d6e4e70a57
sorano12 <3302084363@qq.com>
2024-05-03 13:57:37 +0800

    自旋锁实现

---
+++ SpinLock/SpinLock.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<atomic>
+#include<mingw.thread.h>
+
+int a=0;
+
+class SpinLock{
+public:
+    void lock(){
+        while(flag.test_and_set(std::memory_order_acquire));
+
+    }
+    void unlock(){
+        flag.clear(std::memory_order_release);
+    }
+private:
+    std::atomic_flag flag=ATOMIC_FLAG_INIT;
+};
+
+SpinLock spinlock;
+
+void add_(){
+    spinlock.lock();
+    for(int i=0;i<10000;i++)a++;
+    spinlock.unlock();
+}
+
+int main(){
+    std::thread thread1(add_);
+    std::thread thread2(add_);
+
+    thread1.join();
+    thread2.join();
+
+    std::cout << a << std::endl;
+    return 0;
+
+}
+
